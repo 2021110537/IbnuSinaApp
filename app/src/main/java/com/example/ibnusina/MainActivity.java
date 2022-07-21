cbd9cdea181083db7aae3ba34e082c8f29bf5537
2021110537 <2021110537@student.uitm.edu.my>
2022-07-21 14:39:59 +0800

    Stress

--- app/src/main/java/com/example/ibnusina/MainActivity.java
+++ app/src/main/java/com/example/ibnusina/MainActivity.java
@@ -15,27 +15,11 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private Button button;
     private Button buttonSignin;
-    private Button buttonMap;
-    private Button buttonView;
-    private Button buttonAboutus;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        buttonMap = (Button) findViewById(R.id.buttonMap);
-
-        buttonMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent();
-                intent.setClass(getApplicationContext(),MapsActivity.class);
-                startActivity(intent);
-            }
-
-        });
 
         buttonSignin = (Button) findViewById(R.id.buttonSigin);
         buttonSignin.setOnClickListener(new View.OnClickListener() {
@@ -47,28 +31,5 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-
-        buttonAboutus = (Button) findViewById(R.id.buttonAboutus);
-        buttonAboutus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent();
-                intent.setClass(getApplicationContext(),aboutActivity.class);
-                startActivity(intent);
-            }
-
-        });
-
-        buttonView = (Button) findViewById(R.id.buttonView);
-        buttonView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent();
-                intent.setClass(getApplicationContext(),HospitalListActivity.class);
-                startActivity(intent);
-            }
-
-        });
-
     }
 }
